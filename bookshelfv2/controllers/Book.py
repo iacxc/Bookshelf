11d2637f9e72c0d14913ca97cebb3aca0d0be482
Chengxin Cai <chengxin.cai@gmail.com>
2018-02-09 15:52:38 +0800

    update

--- bookshelfv2/controllers/Book.py
+++ bookshelfv2/controllers/Book.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 
 from pecan import expose
 
@@ -36,8 +38,34 @@ class BookController(object):
             return NullController(id_), remainder
 
     @expose(generic=True, template='json')
+    @expose('booklist.html')
     def index(self):
-        return Book.find_all()
+        return {'resource':
+                    {'register': '注 册',
+                     'login': '登 录',
+                     'logout': '登 出',
+                     'username': '用户名',
+                     'password': '密  码',
+                     'search': '查 找',
+                     'op': '操 作',
+                     'delete': '删除',
+                     'modify': '修改',
+                     'addbook': '添加新书',
+                     'ok': '确 定',
+                     'continue': '继 续',
+                     'reset': '重 置',
+                     'back': '返 回',
+                     'index': '序 号',
+                     'bookname': '书 名',
+                     'bookseries': '系 列',
+                     'bookauthor': '作 者',
+                     'barcode': '条 码',
+                     'bookowner': '所有人',
+                     'bookcreate': '上架时间',
+                     'bookstatus': '当前状态',
+                     'booklastmodified': '最后更新时间',
+                     },
+                'books':Book.find_all()}
 
     # HTTP POST <k=v> ...
     @index.when(method='POST', template='json')
